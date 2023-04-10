395bfe7f2ecb873d83bca24c415d724a86f3a8a9
Arjun Bawa <arj1211@users.noreply.github.com>
2023-04-10 19:04:05 -0400

    a8 makefiles and setup files and simulator

--- A8/CarND-Path-Planning-Project/install-mac.sh
+++ A8/CarND-Path-Planning-Project/install-mac.sh
@@ -5,7 +5,11 @@ cd uWebSockets
 git checkout e94b6e1
 patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
-export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
+export LDFLAGS="-L/opt/homebrew/opt/openssl@3/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/openssl@3/include"
+export OPENSSL_INCLUDE_DIR="/opt/homebrew/opt/openssl@3/include"
+export OPENSSL_CRYPTO_LIBRARY="/opt/homebrew/opt/openssl@3/lib"
 cd build
 OPENSSL_VERSION=`openssl version -v | cut -d' ' -f2`
 cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/$OPENSSL_VERSION/lib ..
---
+++ A8/CarND-Path-Planning-Project/src/CMakeFiles/path_planning.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/arjunbawa/Library/CloudStorage/OneDrive-UniversityofWaterloo/Uni/4B/ECE 495/Assignments/A8/CarND-Path-Planning-Project/src/main.cpp" "CMakeFiles/path_planning.dir/main.cpp.o" "gcc" "CMakeFiles/path_planning.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
